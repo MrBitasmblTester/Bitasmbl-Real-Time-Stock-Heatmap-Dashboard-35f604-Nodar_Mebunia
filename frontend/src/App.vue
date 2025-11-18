7a2b6b95fbf0c5f9335c800ad95419d7918af366
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-18 18:18:50 +0400

    Add App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,6 @@
+<template><div id="app"><Filters @update="onFilter"/><Heatmap :filters="filters"/></div></template>
+<script>
+import Filters from './components/Filters.vue';
+import Heatmap from './components/Heatmap.vue';
+export default { components:{ Filters, Heatmap }, data(){return{filters:{}}}, methods:{ onFilter(f){this.filters = {...this.filters, ...f}} } }
+</script>
